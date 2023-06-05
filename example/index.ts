a7dc01ff612aeef5287ec488a49737f9e1182389
benibana2001 <benibana2001@gmail.com>
2023-06-05 09:44:57 +0900

    refactoring

--- example/index.ts
+++ example/index.ts
@@ -1,18 +1,21 @@
-import {openbd} from "../src/OpenBD.js"
-import {Calil} from "../src/Calil.js"
+import { openbd } from "../src/OpenBD.js";
+import { calil } from "../src/Calil.js";
 
-const isbn = '9784877713379'
+const isbn = "9784877713379";
 
 // OpenBD
-console.log(await openbd.search(isbn))
-
+console.log(await openbd.search(isbn));
 
 // Calil
-const CALIL_KEY ="46a2412f4ceb07b72a251150f2533c74"
-const systemid = 'Tokyo_Setagaya';
+const CALIL_KEY = "46a2412f4ceb07b72a251150f2533c74";
+const systemid = "Tokyo_Setagaya";
+const pollingDuration = 500;
 
-console.log(await new Calil({
-  appkey: CALIL_KEY,
-  isbn,
-  systemid 
-}).search())
+console.log(
+  await calil.search({
+    appkey: CALIL_KEY,
+    isbn,
+    systemid,
+    pollingDuration,
+  })
+);
--- src/Calil.ts
+++ src/Calil.ts
@@ -1,18 +1,27 @@
+/**
+ *
+ * TEST URL - PLEASE SET YOUR APPKEY -
+ *
+ * https://api.calil.jp/check?appkey={}&isbn=4334926940&systemid=Tokyo_Setagaya&format=json
+ * https://api.calil.jp/check?appkey={}&isbn=4834000826&systemid=Aomori_Pref&format=json
+ *
+ */
 export interface LibRequest {
-  appkey: string
-  isbn: string
-  systemid: string
+  appkey: string;
+  isbn: string;
+  systemid: string;
+  pollingDuration: number;
 }
 
 export interface LibResponse {
-  libkey: LibData[]
-  reserveurl: string
+  libkey: LibData[];
+  reserveurl: string;
 }
 
 export interface LibData {
-  libraryID: number
-  libraryName: string
-  bookStatus: string
+  libraryID: number;
+  libraryName: string;
+  bookStatus: string;
 }
 
 enum ServerStatus {
@@ -22,211 +31,181 @@ enum ServerStatus {
   NOT_EXIST = -2,
 }
 
-export class Calil {
+const DEFAULT_LIB_REQUEST: LibRequest = {
+  appkey: "",
+  isbn: "",
+  systemid: "",
+  pollingDuration: 2000,
+};
+
+class Calil {
   //----------------------------------------
-  public api_timeout_timer = 0
-  public api_call_count = 0
-  public data_cache = ''
-  //
-  private readonly HOST: string = 'https://api.calil.jp/check'
-  //
-  private _request: LibRequest
+  public api_timeout_timer = 0;
+  public api_call_count = 0;
+  public data_cache = "";
+
+  private readonly HOST: string = "https://api.calil.jp/check";
+  private _request: LibRequest;
+  private _serverStatus = 0;
+
   get request() {
-    return this._request
+    return this._request;
   }
+
   set request(request: LibRequest) {
-    this._request = request
+    this._request = request;
   }
-  //
-  private _serverStatus = 0
+
   set serverStatus(status: number) {
-    this._serverStatus = status
+    this._serverStatus = status;
   }
   get serverStatus() {
-    return this._serverStatus
+    return this._serverStatus;
   }
-  //
-  private _session = ''
+
+  private _session = "";
   set session(session: string) {
-    this._session = session
+    this._session = session;
   }
   get session() {
-    return this._session
+    return this._session;
   }
-  //
-  private _response: LibResponse | null = null
+
+  private _response: LibResponse | null = null;
   set response(data: LibResponse) {
-    this._response = data
+    this._response = data;
   }
   get response() {
-    return this._response
+    return this._response;
   }
-  //----------------------------------------
-  constructor(req: LibRequest) {
-    // Set Arguments.
-    this._request = req
-    // Check Arguments.
-    this.init()
-  }
-  //----------------------------------------
-  /**
-   * init
-   */
-  private init(): void {
-    this.checkOptions()
-    // THIS IS DEBUG FUNCTION TO BE ERASED!!
-    // this.setTestOptions()
-  }
-  /**
-   * setTestOptions
-   */
-  private setTestOptions(): void {
-    this._request = {
-      appkey: process.env.APP_API_KEY,
-      isbn: '4834000826',
-      systemid: 'Tokyo_Setagaya',
-    }
-  }
+
   /**
    * checkOptions
    * Validate Options each value.
    */
   private checkOptions(): void {
-    // Set AppKey
     if (!this._request.appkey) {
-      alert('Please enter appkey')
+      console.error("Please set APP KEY");
     }
-    // Set ISBN to property.
-    /**
-     * ISBN would be passed as string everytime.
-     */
+    if (typeof this.request.isbn !== "string") {
+      console.error("Please set ISBN as string.");
+    }
+    if (!this._request.isbn) {
+      console.error("Please set ISBN");
+    }
     // Set SystemID to property.
-    /**
-     * User wouldn't enter SystemID by theirself, and SystemID would be assumed inputted by App, so omit validataion.
-     */
+    if (!this._request.systemid) {
+      console.error("Please set SYSTEM ID");
+    }
   }
+
   /**
-   * search
+   * Search book states
    *
-   * Call api using XMLHttpRequest or fetch with JSONP.
-   * If using fetch API, using third party library for JSONP is required
-   * because not supported in standard fetch API.
-   *
+   * Call api using fetch with JSONP.
    */
   public async search(req: LibRequest = this._request): Promise<LibResponse> {
-    // Create url
-    // https://api.calil.jp/check?appkey={}&isbn=4334926940&systemid=Tokyo_Setagaya&format=json
-    // https://api.calil.jp/check?appkey={}&isbn=4834000826&systemid=Aomori_Pref&format=json
-    this._request = req
-    const url: string =
-      this.HOST +
-      '?appkey=' +
-      this._request.appkey +
-      '&isbn=' +
-      this._request.isbn +
-      '&systemid=' +
-      this._request.systemid +
-      '&format=json'
+    this._request = req;
+
+    this.checkOptions();
+
+    const url = `${this.HOST}?appkey=${this._request.appkey}&isbn=${this._request.isbn}&systemid=${this._request.systemid}&format=json`;
 
     // Request
-    const json: any = await this.callApi(url)
-    // Check server status
+    const json: object = await this.callApi(url);
+
     /**
-     * Calis server return status as a number.
-     * We should judge server process would be done or not by checking this status.
+     *
+     * Check server status
+     *
+     * Calis server return status code as a number.
+     * We should judge server process would be done or not by checking this status code.
      * If not finished, we should proceed polling process.
+     *
      */
-    await this.checkServerStatus(json)
-    // Check value 'continue' and decide next process.
-    // this.confirm()
-    // Parse Data
-    return this._response
+    await this.checkServerStatus(json);
+
+    return this._response;
   }
   /**
    * checkServerStatus
    */
-  private async checkServerStatus(json: any): Promise<any> {
+  private async checkServerStatus(json: object): Promise<any> {
     // Set server status
-    this.serverStatus = this.getServerStatus(json)
+    this.serverStatus = this.retrieveStatusCodeFromJSON(json);
     // Set session
-    this.session = this.getSession(json)
-    //
-    // if (this.server_status === 1) {
+
+    this.session = this.retrieveSessionFromJSON(json);
+
     if (this.serverStatus === ServerStatus.POLLING) {
       // Polling
-      await this.sleep(2000)
-      await this.poll()
+      await this.sleep(this.request.pollingDuration);
+      await this.poll();
     } else if (this.serverStatus === ServerStatus.SUCCESS) {
-      // Done
-      // Parse
-      const res: LibResponse = this.parse(json)
-      // Set data
-      this.response = res
-      return
+      this.response = this.retrieveLibraryResponseFromJSON(json);
+      return;
     } else {
       if (this.serverStatus === ServerStatus.NOT_EXIST) {
-        console.error('Error - book is not exist')
+        console.error("Error - book is not exist");
       } else if (this.serverStatus === ServerStatus.SERVER_ERROR) {
-        console.error(`Error - server.status: ${this.serverStatus}`)
+        console.error(`Error - server.status: ${this.serverStatus}`);
       }
-      return
+
+      return;
     }
   }
   /**
    * poll
    */
   public async poll(): Promise<any> {
-    const url: string =
-      this.HOST +
-      '?appkey=' +
-      this._request.appkey +
-      '&session=' +
-      this._session +
-      '&format=json'
+    const url: string = `${this.HOST}?appkey=${this._request.appkey}&session=${this._session}&format=json`;
     // request polling
-    const json: any = await this.callApi(url)
+    const json: object = await this.callApi(url);
     // Check server status
-    await this.checkServerStatus(json)
+    await this.checkServerStatus(json);
   }
 
   /**
-   * parse
    *
    * Parse JSON data to use React JSX easily.
    * When display some data in JSX, you are recommended to use Array.map() function,
    * and raw JSON data is difficult to display. So that this function parse JSON data to array.
+   *
    */
-  public parse(json: any): LibResponse {
+  public retrieveLibraryResponseFromJSON(json: any): LibResponse {
     const libkey: any =
-      json.books[this._request.isbn][this._request.systemid].libkey
+      json.books[this._request.isbn][this._request.systemid].libkey;
     const reserveurl: string =
-      json.books[this._request.isbn][this._request.systemid].reserveurl
-    const res: LibResponse = { libkey: [], reserveurl: reserveurl }
-    let i = 1
+      json.books[this._request.isbn][this._request.systemid].reserveurl;
+    const res: LibResponse = { libkey: [], reserveurl: reserveurl };
+    let i = 1;
     for (const key in libkey) {
-      const d: LibData = { libraryID: i, libraryName: key, bookStatus: libkey[key] }
-      res.libkey.push(d)
-      i++
+      const d: LibData = {
+        libraryID: i,
+        libraryName: key,
+        bookStatus: libkey[key],
+      };
+      res.libkey.push(d);
+      i++;
     }
-    return res
+    return res;
   }
-  /**
-   * callApi
-   */
-  public async callApi(url: string): Promise<any> {
-    let s : object
+
+  public async callApi(url: string): Promise<object> {
+    let s: object;
 
     await fetch(url)
       .then((res) => res.text())
-      .then(resText => {
-        const match = resText.match(/callback\((.*)\);/)
-        if(!match) throw new Error('invalid JSONP response')
-        s = JSON.parse(match[1])
-      }).catch(error => {
-        console.error(error)
-      }) 
+      .then((resText) => {
+        const match = resText.match(/callback\((.*)\);/);
+        if (!match) throw new Error("invalid JSONP response");
+        s = JSON.parse(match[1]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
-    return s
+    return s;
   }
 
   /**
@@ -237,40 +216,43 @@ export class Calil {
    * -1: server error
    * -2: boo isn't exist
    */
-  private getServerStatus(data: any): ServerStatus {
-    const c = data.continue
-    const status = data.books[this._request.isbn][this._request.systemid].status
+  private retrieveStatusCodeFromJSON(data: any): ServerStatus {
+    const c = data.continue;
+    const status =
+      data.books[this._request.isbn][this._request.systemid].status;
     if (c === 1) {
       // return 1
-      return ServerStatus.POLLING
+      return ServerStatus.POLLING;
     } else if (c === 0) {
-      if (status === 'OK' || status === 'Cache') {
+      if (status === "OK" || status === "Cache") {
         const libkey: any =
-          data.books[this._request.isbn][this._request.systemid].libkey
+          data.books[this._request.isbn][this._request.systemid].libkey;
         if (!libkey || !Object.keys(libkey).length) {
           // return -2
-          return ServerStatus.NOT_EXIST
+          return ServerStatus.NOT_EXIST;
         } else {
           // return 0
-          return ServerStatus.SUCCESS
+          return ServerStatus.SUCCESS;
         }
       } else {
         // return -1
-        return ServerStatus.SERVER_ERROR
+        return ServerStatus.SERVER_ERROR;
       }
     }
   }
   /**
    * getSession
    */
-  private getSession(data: any): string {
-    return data.session
+  private retrieveSessionFromJSON(data: any): string {
+    return data.session;
   }
 
   /**
    * sleep
    */
   private sleep(ms: number): Promise<any> {
-    return new Promise((resolve) => setTimeout(resolve, ms))
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
+
+export const calil = new Calil();
