60adb1bf3324367870a4cb5d756fbe47fa3ef86d
benibana2001 <benibana2001@gmail.com>
2023-07-07 13:20:12 +0900

    define prefecture names

--- src/Calil.ts
+++ src/Calil.ts
@@ -39,7 +39,7 @@ export interface CalilRequest {
    * The string to be specified must be the one determined by Calil
    * One string is assigned to each municipality.
    */
-  systemid: string;
+  systemid: Prefecture;
 
   /**
    * ポーリングの間隔(ms)
@@ -59,21 +59,55 @@ export interface CalilResponse {
    *
    * continueが1で返ってきたときは、クライアントは戻り値のsessionをパラメータにして、再度checkをリクエストします。
    */
-  continue: number;
+  continue: CALIL_SERVER_CONTINUE_STATUS;
 
-  books: CalilResponseBook[];
+  books: CalilResponseBook;
 }
 
 type CalilResponseBook = {
-  string: CalilResponsePrefecture;
+  // Key is ISBN you requested
+  [key: string]: CalilResponsePrefecture;
 };
 
+// type CalilResponsePrefecture = {
+//   [key: Prefecture]: {
+//     status: CalilResponseSearchStatus;
+//     reserveurl: string;
+//     libkey: {
+//       [key: string]: BorrowingStatus;
+//     };
+//   };
+// };
+// type CalilResponsePrefecture = {
+//   [key: Prefecture]: {
+//     status: CalilResponseSearchStatus;
+//     reserveurl: string;
+//     libkey: {
+//       [key: string]: BorrowingStatus;
+//     };
+//   };
+// };
 type CalilResponsePrefecture = {
-  string: {
-    status: string;
+  [key: string]: {
+    status: CalilResponseSearchStatus;
     reserveurl: string;
+    libkey: CalilResponseLibkey;
   };
 };
+
+type CalilResponseSearchStatus = 'OK' | 'Cache' | 'Running' | 'Error';
+
+type CalilResponseLibkey = { [key: string]: BorrowingStatus };
+
+type BorrowingStatus =
+  | '貸出可'
+  | '蔵書あり'
+  | '館内のみ'
+  | '貸出中'
+  | '予約中'
+  | '準備中'
+  | '休館中'
+  | '蔵書なし';
 
 /**
  *
@@ -84,11 +118,6 @@ export interface ParsedResponse {
   session?: string;
 
   continue: number;
-
-  /**
-   *
-   */
-  books: object;
 
   /**
    *
@@ -117,7 +146,7 @@ export interface LibData {
    * 検索した本の貸出状況
    * Borrowing status of searched books
    */
-  borrowingStatus: string;
+  borrowingStatus: BorrowingStatus;
 }
 
 /**
@@ -125,6 +154,8 @@ export interface LibData {
  * State of book search on Calil server
  */
 type CALIL_SERVER_BOOK_STATUS = 0 | 1 | -1 | -2;
+
+type CALIL_SERVER_CONTINUE_STATUS = 0 | 1;
 
 const ServerStatus: { [key: string]: CALIL_SERVER_BOOK_STATUS } = {
   SUCCESS: 0,
@@ -136,14 +167,24 @@ const ServerStatus: { [key: string]: CALIL_SERVER_BOOK_STATUS } = {
 const DEFAULT_CALIL_REQUEST: CalilRequest = {
   appkey: '',
   isbn: '',
-  systemid: '',
+  systemid: 'Tokyo_Setagaya',
   pollingDuration: 2000
 };
 
 const DEFAULT_CALIL_RESPONSE: CalilResponse = {
   session: '',
-  continue: 0 | 1,
-  books: []
+  continue: 0,
+  books: {
+    '4334926940': {
+      Tokyo_Setagaya: {
+        status: 'OK',
+        libkey: {
+          xxx: '貸出中'
+        },
+        reserveurl: ''
+      }
+    }
+  }
 };
 
 /**
@@ -284,17 +325,14 @@ export class Calil {
    *
    */
   private retrieveLibraryResponseFromJSON(json: CalilResponse): ParsedResponse {
-    const libkey: any =
+    const libkey: CalilResponseLibkey =
       json.books[this._request.isbn][this._request.systemid].libkey;
     const reserveurl: string =
       json.books[this._request.isbn][this._request.systemid].reserveurl;
-    const books: object =
-      json.books[this._request.isbn][this._request.systemid].books;
     const res: ParsedResponse = {
       libkey: [],
       reserveurl,
-      continue: 0,
-      books
+      continue: 0
     };
     let i = 1;
     for (const key in libkey) {
@@ -340,16 +378,18 @@ export class Calil {
    *
    * サーバーから返却されたcontinueの値を見て処理を分岐・実行します。
    */
-  private retrieveStatusCodeFromJSON(data: CalilResponse): CALIL_SERVER_BOOK_STATUS {
-    const status =
+  private retrieveStatusCodeFromJSON(
+    data: CalilResponse
+  ): CALIL_SERVER_BOOK_STATUS {
+    const searchStatus =
       data.books[this._request.isbn][this._request.systemid].status;
 
     if (data.continue === 1) {
       return ServerStatus.POLLING;
     } else if (data.continue === 0) {
       // 蔵書の有無を判定
-      if (status === 'OK' || status === 'Cache') {
-        const libkey: any =
+      if (searchStatus === 'OK' || searchStatus === 'Cache') {
+        const libkey: CalilResponseLibkey =
           data.books[this._request.isbn][this._request.systemid].libkey;
 
         if (!libkey || !Object.keys(libkey).length) {
@@ -366,9 +406,127 @@ export class Calil {
     return ServerStatus.SERVER_ERROR;
   }
 
-  private sleep(ms: number): Promise<any> {
+  private sleep(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
+
+type PrefecturePrefix = 'Tokyo_';
+type PrefectureName =
+  | 'Adachi'
+  | 'Akiruno'
+  | 'Akishima'
+  | 'Arakawa'
+  | 'Bunkyo'
+  | 'Chiyoda'
+  | 'Chofu'
+  | 'Chuo'
+  | 'Edogawa'
+  | 'Fuchu'
+  | 'Fussa'
+  | 'Hachijo'
+  | 'Hachioji'
+  | 'Hamura'
+  | 'Higashikurume'
+  | 'Higashimurayama'
+  | 'Higashiyamato'
+  | 'Hino'
+  | 'Hinode'
+  | 'Inagi'
+  | 'Itabashi'
+  | 'Katsushika'
+  | 'Kita'
+  | 'Kiyose'
+  | 'Kodaira'
+  | 'Koganei'
+  | 'Kokubunji'
+  | 'Komae'
+  | 'Koto'
+  | 'Kunitachi'
+  | 'Machida'
+  | 'Meguro'
+  | 'Minato'
+  | 'Mitaka'
+  | 'Mizuho'
+  | 'Musashimurayama'
+  | 'Musashino'
+  | 'Nakano'
+  | 'NDL'
+  | 'Nerima'
+  | 'Niijima'
+  | 'Nishitokyo'
+  | 'Okutama'
+  | 'Ome'
+  | 'Ota'
+  | 'Koganei'
+  | 'Setagaya'
+  | 'Shibuya'
+  | 'Shinagawa'
+  | 'Shinjuku'
+  | 'Suginami'
+  | 'Sumida'
+  | 'Tachikawa'
+  | 'Taito'
+  | 'Tama'
+  | 'Toshima';
+type Prefecture = `${PrefecturePrefix}${PrefectureName}`;
+
+export const PrefectureList: [Prefecture, string][] = [
+  ['Tokyo_Adachi', '足立区'],
+  ['Tokyo_Akiruno', 'あきる野市'],
+  ['Tokyo_Akishima', '昭島市'],
+  ['Tokyo_Arakawa', '荒川区'],
+  ['Tokyo_Bunkyo', '文京区'],
+  ['Tokyo_Chiyoda', '千代田区'],
+  ['Tokyo_Chofu', '調布市'],
+  ['Tokyo_Chuo', '中央区'],
+  ['Tokyo_Edogawa', '江戸川区'],
+  ['Tokyo_Fuchu', '府中市'],
+  ['Tokyo_Fussa', '福生市'],
+  ['Tokyo_Hachijo', '八丈町'],
+  ['Tokyo_Hachioji', '八王子市'],
+  ['Tokyo_Hamura', '羽村市'],
+  ['Tokyo_Higashikurume', '東久留米市'],
+  ['Tokyo_Higashimurayama', '東村山市'],
+  ['Tokyo_Higashiyamato', '東大和市'],
+  ['Tokyo_Hino', '日野市'],
+  ['Tokyo_Hinode', '日の出町'],
+  ['Tokyo_Inagi', '稲城市'],
+  ['Tokyo_Itabashi', '板橋区'],
+  ['Tokyo_Katsushika', '葛飾区'],
+  ['Tokyo_Kita', '北区'],
+  ['Tokyo_Kiyose', '清瀬市'],
+  ['Tokyo_Kodaira', '小平市'],
+  ['Tokyo_Koganei', '小金井市'],
+  ['Tokyo_Kokubunji', '国分寺市'],
+  ['Tokyo_Komae', '狛江市'],
+  ['Tokyo_Koto', '江東区'],
+  ['Tokyo_Kunitachi', '国立市'],
+  ['Tokyo_Machida', '町田市'],
+  ['Tokyo_Meguro', '目黒区'],
+  ['Tokyo_Minato', '港区'],
+  ['Tokyo_Mitaka', '三鷹市'],
+  ['Tokyo_Mizuho', '瑞穂町'],
+  ['Tokyo_Musashimurayama', '武蔵村山市'],
+  ['Tokyo_Musashino', '武蔵野市'],
+  ['Tokyo_Nakano', '中野区'],
+  ['Tokyo_NDL', '国立国会図書館'],
+  ['Tokyo_Nerima', '練馬区'],
+  ['Tokyo_Niijima', '新島村'],
+  ['Tokyo_Nishitokyo', '西東京市'],
+  ['Tokyo_Okutama', '奥多摩町'],
+  ['Tokyo_Ome', '青梅市'],
+  ['Tokyo_Ota', '大田区'],
+  ['Tokyo_Setagaya', '世田谷区'],
+  ['Tokyo_Shibuya', '渋谷区'],
+  ['Tokyo_Shinagawa', '品川区'],
+  ['Tokyo_Shinjuku', '新宿区'],
+  ['Tokyo_Suginami', '杉並区'],
+  ['Tokyo_Sumida', '墨田区'],
+  ['Tokyo_Tachikawa', '立川市'],
+  ['Tokyo_Taito', '台東区'],
+  ['Tokyo_Tama', '多摩市'],
+  ['Tokyo_Toshima', '豊島区']
+];
 
 export const calil = new Calil();
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { OpenBD, openbd } from './OpenBD';
 import { Calil, CalilRequest, calil } from './Calil';
 
+/**
+ * 
+ * ISBNをもとに書籍の情報を取得する
+ * - 書影、タイトル、
+ * - 図書館の在庫
+ * 
+ * 
+ * Retrieve information about a book based on its ISBN
+ * - Book cover imate, title,
+ * - Book library (@Tokyo) inventory
+ * 
+ */
 export class BeniBook {
   private _openbd: OpenBD;
   private _calil: Calil;
@@ -10,10 +22,16 @@ export class BeniBook {
     this._calil = calil;
   }
 
+  /**
+   * 本の書影を取得する
+   */
   public async searchBookCoverURL(isbn: string) {
     return (await this._openbd.search(isbn)).coverurl;
   }
 
+  /**
+   * 本のタイトルを取得する
+   */
   public async searchBookTitle(isbn: string) {
     return (await this._openbd.search(isbn)).title;
   }
